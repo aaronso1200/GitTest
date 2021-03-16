550c0eae802fb2f707da1d2ca0479cb6e90d4f4b
aaron so <aaronso1200@gmail.com>
2021-03-16 22:54:51 +0800

    Initial commit

---
+++ WebApplication1/Controllers/AaronLuck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AaronLuck : ControllerBase
+    {
+        [HttpGet]
+        public List<object> Get()
+        {
+            SqlBuilder sqlBuilder = new SqlBuilder();
+            SqlBuilder.Template builder = new SqlBuilder().AddTemplate("SELECT * from user_table");
+            var ormModule = new BaseDapper();
+            List<object> rs = ormModule.SelectTable<object>(builder.RawSql, builder.Parameters);
+
+            return rs;
+        }
+    }
+}
---
+++ WebApplication1/Services/Dapper.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper;
+using Dapper.Contrib;
+using Dapper.Contrib.Extensions;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace WebApplication1.Services
+{
+    public class BaseDapper
+    {
+        private string _connStr = "";
+
+        private string GetSQLConnection()
+        {
+            if (this._connStr != "") return this._connStr;
+            
+            string host = "DESKTOP-40VK2HB";
+            string instance = "SQLEXPRESS";
+            string dbName = "TestDB";
+            string user = "sa";
+            string password = "password";
+
+            this._connStr =
+                $"Persist Security Info=False;Data Source={host}\\{instance};User Id={user};Password={password};Initial Catalog={dbName}";
+            return this._connStr;
+        }
+
+        public List<T> SelectTable<T>(string strSql, object selectorParameters)
+        {
+            List<T> results = null;
+            using (SqlConnection conn = new SqlConnection(this.GetSQLConnection()))
+            {
+                results = conn.Query<T>(strSql, selectorParameters).ToList();
+            }
+
+            return results;
+        }
+
+        public bool Update<T>(T obj) where T : class
+        {
+            using (SqlConnection conn = new SqlConnection(this.GetSQLConnection()))
+            {
+                conn.Open();
+                return conn.Update(obj);
+            }
+        }
+
+        public bool Update<T>(IEnumerable<T> obj) where T : class
+        {
+            using (SqlConnection conn = new SqlConnection(this.GetSQLConnection()))
+            {
+                conn.Open();
+                return conn.Update(obj);
+            }
+        }
+
+        public long Insert<T>(IEnumerable<T> obj) where T : class
+        {
+            using (SqlConnection conn = new SqlConnection(this.GetSQLConnection()))
+            {
+                conn.Open();
+                return conn.Insert(obj);
+            }
+        }
+
+
+        public long Insert<T>(T obj) where T : class
+        {
+            using (SqlConnection conn = new SqlConnection(this.GetSQLConnection()))
+            {
+                conn.Open();
+                return conn.Insert(obj);
+            }
+        }
+    }
+}
