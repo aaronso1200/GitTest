6ea4abf834a6b87b829f1a48c47de5eb261a74dc
aaron so <aaronso1200@gmail.com>
2021-03-16 23:00:42 +0800

    Commit some change blah blah

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -23,6 +23,7 @@ namespace WebApplication1
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        //LKJKLJSDF
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
